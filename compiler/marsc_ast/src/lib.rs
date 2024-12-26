91b1e5cc148a62bffc18cf902370f7dc4768404a
Makar A. Pestrikov <mpxx088@gmail.com>
2024-12-26 18:03:15 +0300

    pest file added

--- compiler/marsc_ast/src/lib.rs
+++ compiler/marsc_ast/src/lib.rs
@@ -1,3 +1,8 @@
-pub mod ast;
-pub mod expr;
-pub mod stmt;
+use anyhow::Result;
+use pest::Parser as PestParser;
+use pest_derive::Parser as PestParser;
+
+
+#[derive(PestParser)]
+#[grammar = "mars_grammar.pest"]
+struct Parser;
