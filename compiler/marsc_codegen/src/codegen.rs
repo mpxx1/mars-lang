46c08a8666fe566bed865fc607428777a5f4631a
Никита Загута <65826492+XPlaGo@users.noreply.github.com>
2025-01-22 13:49:43 +0300

    Revert;

--- compiler/marsc_codegen/src/codegen.rs
+++ compiler/marsc_codegen/src/codegen.rs
@@ -1,17 +1,14 @@
-use std::cell::RefCell;
 use ast::{Block, Expr, FuncCall, Identifier, Literal, LogicalExpr, MathExpr, Stmt, Type};
 use inkwell::builder::Builder;
 use inkwell::context::Context;
 use inkwell::module::Module;
-use inkwell::targets::{FileType, InitializationConfig, Target, TargetMachine};
 use inkwell::types::{BasicMetadataTypeEnum, BasicType, BasicTypeEnum};
-use inkwell::values::{BasicMetadataValueEnum, BasicValue, BasicValueEnum, FunctionValue, PointerValue};
-use inkwell::AddressSpace;
+use inkwell::values::{BasicMetadataValueEnum, BasicValue, BasicValueEnum, PointerValue};
 use mir::{FuncProto, Mir, Scope, StructProto};
 use std::collections::HashMap;
 use std::path::PathBuf;
-use std::process::Command;
-use std::rc::Rc;
+use inkwell::AddressSpace;
+use inkwell::targets::{FileType, InitializationConfig, Target, TargetMachine};
 
 pub struct Codegen<'ctx, 'src> {
     pub(crate) context: &'ctx Context,
@@ -28,7 +25,7 @@ where
     pub(crate) fn codegen(&mut self) -> String {
         let global_scope: &'ctx Scope = self.mir.scopes.get(&0_usize).unwrap();
         self.codegen_scope(global_scope);
-        
+
         self.module.print_to_string().to_string()
     }
 
@@ -39,30 +36,15 @@ where
 
     pub(crate) fn codegen_scope(&mut self, scope: &'ctx Scope<'src>) {
 
-        let mut self_ptr = Rc::new(RefCell::new(&mut *self));
-        let mut self_temp = self_ptr.borrow();
-
-
         for scope_struct in &scope.structs {
-            self_temp.codegen_struct(scope_struct.1);
+            self.codegen_struct(scope_struct.1);
         }
 
-        &scope.funs
-            .iter()
-            .filter(|key| self_temp.mir.sys_funs.contains(&key.1.ident))
-            .map(|key| self_temp.generate_external_function(&key.1));
-
-        let mut functions_definitions: Vec<(&FuncProto, FunctionValue)> = vec![];
-        for (_, func_proto) in scope.funs
-            .iter()
-            .filter(|key| !self_temp.mir.sys_funs.contains(&key.1.ident)) {
-            let value = self_temp.codegen_function_definition(func_proto);
-            functions_definitions.push((func_proto, value));
-        }
-
-        while functions_definitions.len() > 0 {
-            let (func_proto, func_value) = functions_definitions.pop().unwrap();
-            self.as_.codegen_function_entry(func_proto, func_value);
+        for scope_fun in &scope.funs {
+            match scope_fun.1.return_type {
+                Type::Void => self.codegen_void_func(scope_fun.1),
+                _ => self.codegen_func(scope_fun.1),
+            }
         }
 
         for stmt in &scope.instrs {
@@ -72,24 +54,21 @@ where
 
     pub(crate) fn codegen_struct(&self, struct_decl: &'ctx StructProto<'src>) {
         let struct_type = self.context.opaque_struct_type(struct_decl.ident);
-        
+
         let field_types: Vec<BasicTypeEnum> = struct_decl
             .fields
             .iter()
             .map(|field| self.codegen_type(&field.ty))
             .collect();
-        
+
         struct_type.set_body(&field_types, false);
     }
 
-    pub(crate) fn codegen_function_definition(&mut self, func_decl: &'ctx FuncProto<'src>) {
+    pub(crate) fn codegen_func(&mut self, func_decl: &'ctx FuncProto<'src>) {
 
         if self.mir.sys_funs.contains(&func_decl.ident) {
             self.generate_external_function(func_decl);
-        }
-
-        if func_decl.return_type == Type::Void {
-            return self.codegen_void_function_definition(func_decl);
+            return;
         }
 
         let arg_types: Vec<BasicMetadataTypeEnum> = func_decl
@@ -97,20 +76,23 @@ where
             .iter()
             .map(|arg| self.codegen_type(&arg.ty).into())
             .collect();
-        
+
         let return_type = self.codegen_type(&func_decl.return_type);
         let fn_type = return_type.fn_type(&arg_types, false);
-        self.module.add_function(&func_decl.ident, fn_type, None)
+        let function = self.module.add_function(&func_decl.ident, fn_type, None);
+        let entry = self.context.append_basic_block(function, "entry");
+        self.builder.position_at_end(entry);
 
-        self.genera
+        self.codegen_scope_by_id(&func_decl.node_id);
     }
 
-    pub(crate) fn codegen_void_function_definition(&mut self, func_decl: &'ctx FuncProto<'src>) {
-
+    pub(crate) fn codegen_void_func(&mut self, func_decl: &'ctx FuncProto<'src>) {
+        
         if self.mir.sys_funs.contains(&func_decl.ident) {
             self.generate_external_function(func_decl);
+            return;
         }
-
+        
         let arg_types: Vec<BasicMetadataTypeEnum> = func_decl
             .args
             .iter()
@@ -119,14 +101,12 @@ where
 
         let return_type = self.context.void_type();
         let fn_type = return_type.fn_type(&arg_types, false);
-        self.module.add_function(&func_decl.ident, fn_type, None)
-    }
-
-    pub(crate) fn codegen_function_entry(&mut self, func_proto: &'ctx FuncProto<'src>, function_value: FunctionValue) {
-        let entry = self.context.append_basic_block(function_value, "entry");
+        let function = self.module.add_function(&func_decl.ident, fn_type, None);
+        let entry = self.context.append_basic_block(function, "entry");
         self.builder.position_at_end(entry);
 
-        self.codegen_scope_by_id(&func_proto.node_id);
+        self.codegen_scope_by_id(&func_decl.node_id);
+
         self.builder.build_return(None).unwrap();
     }
 
@@ -161,7 +141,7 @@ where
                 let value = self.codegen_expr(expr, scope);
 
                 self.builder.build_store(variable, value).unwrap();
-                
+
                 self.var_table.insert(ident, variable);
             }
             Stmt::Assign { .. } => {}
@@ -180,7 +160,7 @@ where
             Expr::FuncCall(func_call) => {
                 if let Some(value) = self.codegen_func_call(func_call, scope) {
                     value
-                } else { 
+                } else {
                     panic!("Function has no return value")
                 }
             },
@@ -199,7 +179,7 @@ where
 
     pub(crate) fn codegen_identifier(&self, identifier: &'ctx Identifier<'src>, scope: &'ctx Scope<'ctx>) -> BasicValueEnum<'ctx> {
         let variable = scope.vars.get(identifier.ident).unwrap();
-        
+
         if let Some(ptr) = self.var_table.get(identifier.ident) {
             let ty = self.codegen_type(&variable.ty);
             self.builder.build_load(ty, *ptr, identifier.ident).unwrap()
@@ -254,21 +234,21 @@ where
         let function = self.module
             .get_function(func_call.ident.ident)
             .expect(error_msg.as_str());
-        
+
         let args: Vec<BasicMetadataValueEnum> = func_call
             .args
             .iter()
             .map(|arg| self.codegen_expr(arg, scope).into())
             .collect();
-        
+
         let call_site = self.builder.build_call(function, &args, "call");
-        
+
         if let Some(_) = function.get_type().get_return_type() {
             let value = call_site
                 .unwrap().try_as_basic_value()
                 .left()
                 .unwrap();
-            
+
             Some(value)
         } else {
             call_site.unwrap();
@@ -297,9 +277,9 @@ where
 
     pub fn codegen_bytecode(&self, output: &PathBuf) -> Result<(), String> {
         self.module.write_bitcode_to_path(output);
-        
+
         Target::initialize_all(&InitializationConfig::default());
-        
+
         // Получаем целевую платформу
         let target_triple = TargetMachine::get_default_triple();
         let target = Target::from_triple(&target_triple).map_err(|e| e.to_string())?;
@@ -313,17 +293,17 @@ where
                 inkwell::targets::CodeModel::Default,
             )
             .ok_or_else(|| "Failed to create target machine".to_string())?;
-        
+
         // Генерация объектного файла
         target_machine
             .write_to_file(&self.module, FileType::Object, output.as_ref())
             .map_err(|e| e.to_string())
     }
 
-    
+
 }
 
-pub fn codegen<'src>(mir: &'src Mir, output: PathBuf) -> String {
+pub fn codegen<'src>(mir: &'src Mir) -> String {
     let context = Context::create();
     let module = context.create_module("test_module");
     let mut codegen = Codegen::<'_, 'src> {
@@ -336,20 +316,10 @@ pub fn codegen<'src>(mir: &'src Mir, output: PathBuf) -> String {
 
     let result = codegen.codegen();
 
-    let object_file = "/users/nzaguta/RustroverProjects/mars-lang/examples/test.o";
-    let std_libio = "/users/nzaguta/mars/std/libio.so";
-    
-    codegen.codegen_bytecode(&PathBuf::from(object_file)).unwrap();
+    // let out_dir = env::var("OUT_DIR").unwrap();
+    // let output = out_dir + "/program.o";
 
-    let status = Command::new("clang")
-        .args([std_libio, object_file])
-        .args(["-o", "/users/nzaguta/RustroverProjects/mars-lang/examples/test"])
-        .status()
-        .expect("Failed to invoke C compiler and build executable file");
-
-    if !status.success() {
-        panic!("Status is not success");
-    }
+    // codegen.codegen_bytecode(&PathBuf::from(output)).unwrap();
 
     result
 }
